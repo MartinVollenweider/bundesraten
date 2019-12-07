bbee8bd54a93f757be1873b1d58a444051480d69
martinVollenweider <martin.vollenweider@htwchur.ch>
2019-12-07 21:14:24 +0100

    Einheitliche Kommentare, unnötige Doppelklick-Funktion gelöscht

--- js/control.js
+++ js/control.js
@@ -1,9 +1,5 @@
 // Wenn das Dokument geladen wurde, führe loadFunc() aus
 document.addEventListener('DOMContentLoaded', loadFunc);
-/*  Benötigt um herauszufinden, ob man nach Doppelclick auf Bildschirm
-    klickt um die Handlers wieder zu zeigen */
-var doppelclick = false;
-
 // Lade und zeige alle Objekte
 function loadFunc() {
   // Das Registrationsformular
@@ -19,7 +15,7 @@ function loadFunc() {
 
   var tipped = document.querySelector('#tipped');
   if (tipped) {
-    showTippFunc()
+    showTippFunc();
   }
 } // Ende loadFunc
 
@@ -45,7 +41,6 @@ function insertUserFunc(ereignis) {
     .then(function (data) {
       /*  Erhalte als Antwort alle Objekte
           und ersetze das aktuelle HTML mit dem neuen vom Server */
-
       console.log(data);
       document.querySelector('#show').innerHTML = data;
       document.querySelector('#show').style.visibility = 'visible';
@@ -53,7 +48,7 @@ function insertUserFunc(ereignis) {
     .catch(function (error) {
       console.log('Request failed', error);
     }) // Ende fetch
-}  // Ende insertFunc
+}  // Ende insertUserFunc
 
 // Einen neuen Tipp einfügen
 function insertTippFunc(ereignis) {
@@ -70,7 +65,8 @@ function insertTippFunc(ereignis) {
   fetch(url, {
     method: 'POST',
     body: data
-  }).then(function (response) {
+  })
+    .then(function (response) {
       return response.text()
     })
     .then(function (data) {
@@ -82,7 +78,7 @@ function insertTippFunc(ereignis) {
     .catch(function (error) {
       console.log('Request failed', error);
     }) // Ende fetch
-}  // Ende insertFunc
+}  // Ende insertTippFunc
 
 // Einen Tipp holen
 function showTippFunc() {
@@ -92,9 +88,9 @@ function showTippFunc() {
   // Fetch: Sende die Daten per AJAX an den Server
   fetch(url, {
     method: 'GET'
-  }).then(function (response) {
+  })
+    .then(function (response) {
       console.log(response);
-
       return response.text()
     })
     .then(function (data) {
@@ -116,9 +112,8 @@ function showTippFunc() {
       document.querySelector('#tipp6stimmen').innerHTML = tipp.tipp6stimmen;
       document.querySelector('#tipp7').innerHTML = tipp.tipp7;
       document.querySelector('#tipp7stimmen').innerHTML = tipp.tipp7stimmen;
-
     })
     .catch(function (error) {
       console.log('Request failed', error);
     }) // Ende fetch
-}  // Ende insertFunc
+}  // Ende showTippFunc
